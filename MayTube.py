8e908d3bbdaa255e289088c1eb0fd5a6d84dec81
Mayank Gupta <43340190+MayankFawkes@users.noreply.github.com>
2020-07-09 17:18:06 +0530

    Update MayTube.py

--- MayTube.py
+++ MayTube.py
@@ -1,54 +1,43 @@
 import requests, re, os
 from time import sleep
-class YouTube():
-	def __init__(self,link=""):
-		videos={}
-		url="https://y2mate.com/analyze/ajax"
-		headers={
-			"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.",
-			"Referer": "https://y2mate.com/",
-			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-		}
-		data={
-			"url": "https://www.youtube.com/watch?v={}".format(link),
-			"ajax": "1"
-		}
-		yes=requests.post(url, data=data, headers=headers)
-		data=(yes.json()["result"])
-		self.find=re.findall("_id: '(.*?)'", data)
-	def Download(self,ftype="",fquality=""):
-		url="https://y2mate.com/convert"
-		headers={
-			"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.",
-			"Referer": "https://y2mate.com/",
-			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-		}
-		data={
-			"type":"youtube",
-			"_id":"5bfb2d467527f8ed778b4568",
-			"v_id":"HVl0zT0fXes",
-			"ajax":"1",
-			"ftype":"mp4",
-			"fquality":"720"
-		}
-		data["_id"]=self.find[0]
-		data["v_id"]=self.find[1]
-		data["ftype"]=ftype
-		data["fquality"]=fquality
-		yes=requests.post(url, data=data, headers=headers)
-		data=(yes.json()["result"])
-		find=re.findall('<a href="(.*?)"', data)
-		#print(find)
-		if(find):
-			if("y2mate" in find[0].split(".")):
-				return (find[0])
-		else:
-			yes=requests.post(url, data=data, headers=headers)
-			data=(yes.json()["result"])
-			find=re.findall('<a href="(.*?)"', data)
-			#print(find)
-			if(find):
-				if("y2mate" in find[0].split(".")):
-					return(find[0])
-			else:
-				return "try again"
+from json import loads
+from random import choice
+from download import download
+
+class y2mate(download):
+	Generate="https://{}.y2mate.com/analyze/ajax"
+	Convert="https://{}.y2mate.com/convert"
+	Headers={
+		"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.",
+		"Referer": "https://y2mate.com/",
+		"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+	}
+	def __init__(self,vid:str,ftype:str="mp4",fquality:str="720p")->None:
+		self.ftype=ftype
+		self.fquality=fquality
+		self.v_id=vid
+		self._id,self.v_id=self._videourl()
+		self._videourl(True)
+	def _save(self,status:bool=True)-> bool:
+		super().__init__(url=self.videolink,name=self.name,status=status)
+		return True
+	def _server(self,new:bool=True):
+		if new:
+			self.Data_Gen={"url": f"https://www.youtube.com/watch?v={self.v_id}","q_auto": 0,"ajax": "1"}
+		if not new:
+			self.Data_Convert={"type":"youtube","_id":self._id,"v_id":self.v_id,"ajax":"1","ftype":self.ftype,"fquality":self.fquality}
+		# self.sub="mate"+str(choice([f"{i:02d}" for i in range(1,12)]))
+		self.sub="mate06"
+	def _videourl(self,new:bool=False)->list:
+		if not new:
+			self._server()
+			gen=requests.post(self.Generate.format(self.sub), data=self.Data_Gen, headers=self.Headers).text
+			self.name=re.findall('var data_vtitle = "(.*?)";', gen.replace("\\",""))[0]
+			return re.findall("_id: '(.*?)'", gen)
+		if new:
+			self._server(False)
+			res=requests.post(self.Convert.format(self.sub), data=self.Data_Convert, headers=self.Headers)
+			self.name=f'{self.name}.{self.ftype}'.replace("|","")
+			print(self.Data_Convert)
+			print(loads(res.text))
+			self.videolink=re.findall('<a href="(.*?)"',loads(res.text)['result'])[0]
